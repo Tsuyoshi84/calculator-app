f13fcd437adc0d56b742f898c35b0de2e34b6723
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-01-03 12:09:00 +0900

    Add unit tests

---
+++ src/stores/calculation.test.ts
@@ -0,0 +1,120 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { get } from 'svelte/store';
+import {
+	displayValue,
+	addNumber,
+	addDecimal,
+	addOperator,
+	calculate,
+	remove,
+	reset
+} from './calculation';
+
+describe('calculation', () => {
+	beforeEach(() => {
+		reset();
+	});
+
+	it('should show nothing first', () => {
+		expect(get(displayValue)).toBe('');
+	});
+
+	it('should show number after adding', () => {
+		addNumber('1');
+		expect(get(displayValue)).toBe('1');
+
+		addNumber('2');
+		expect(get(displayValue)).toBe('12');
+
+		addNumber('3');
+		expect(get(displayValue)).toBe('123');
+	});
+
+	it('should show number after adding decimal', () => {
+		addNumber('1');
+		addDecimal();
+		addNumber('2');
+		expect(get(displayValue)).toBe('1.2');
+	});
+
+	it('should show add symbol', () => {
+		addNumber('1');
+		addOperator('add');
+		expect(get(displayValue)).toBe('1 +');
+	});
+
+	it('should show subtract symbol', () => {
+		addNumber('1');
+		addOperator('subtract');
+		expect(get(displayValue)).toBe('1 -');
+	});
+
+	it('should show divide symbol', () => {
+		addNumber('1');
+		addOperator('divide');
+		expect(get(displayValue)).toBe('1 /');
+	});
+
+	it('should show divide symbol', () => {
+		addNumber('1');
+		addOperator('multiply');
+		expect(get(displayValue)).toBe('1 x');
+	});
+
+	it('should add two numbers', () => {
+		addNumber('1');
+		addOperator('add');
+		addNumber('2');
+		calculate();
+		expect(get(displayValue)).toBe('3');
+	});
+
+	it('should subtract two numbers', () => {
+		addNumber('1');
+		addOperator('subtract');
+		addNumber('2');
+		calculate();
+		expect(get(displayValue)).toBe('-1');
+	});
+
+	it('should multiply two numbers', () => {
+		addNumber('2');
+		addOperator('multiply');
+		addNumber('6');
+		calculate();
+		expect(get(displayValue)).toBe('12');
+	});
+
+	it('should divide two numbers', () => {
+		addNumber('8');
+		addOperator('divide');
+		addNumber('2');
+		calculate();
+		expect(get(displayValue)).toBe('4');
+	});
+
+	it('should prioritize multiply and divide', () => {
+		// 2 + 3 x 4 / 2 - 1 = 7
+		addNumber('2');
+		addOperator('add');
+		addNumber('3');
+		addOperator('multiply');
+		addNumber('4');
+		addOperator('divide');
+		addNumber('2');
+		addOperator('subtract');
+		addNumber('1');
+		calculate();
+		expect(get(displayValue)).toBe('7');
+	});
+
+	it('should remove last character', () => {
+		addNumber('1');
+		addNumber('2');
+		remove();
+		expect(get(displayValue)).toBe('1');
+
+		remove();
+		expect(get(displayValue)).toBe('');
+	});
+});
