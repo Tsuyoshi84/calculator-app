991e1e8eafe7d8d126a747885ad5254dc1d0aee8
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-01-03 00:43:41 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
 
-export default defineConfig({
-	plugins: [
-		svelte({
-			/* plugin options */
-		})
-	]
-});
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()]
+};
+
+export default config;
