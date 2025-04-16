7111e23788d991428c4cf13a92453d1f76e01e74
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-04-16 19:37:15 +0900

    Update packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginSvelte from 'eslint-plugin-svelte';
 import svelteParser from 'svelte-eslint-parser';
 import tsEslint from 'typescript-eslint';
@@ -9,7 +8,6 @@ export default tsEslint.config(
 	js.configs.recommended,
 	...tsEslint.configs.recommended,
 	...eslintPluginSvelte.configs['flat/recommended'],
-	eslintConfigPrettier,
 	...eslintPluginSvelte.configs['flat/prettier'],
 	{
 		files: ['**/*.svelte'],
